48cd98ef6362b261305cbb6defa6efee6bb1fe01
a-pohlman <a-pohlman@onu.edu>
2026-02-09 19:33:25 -0500

    Delete: TDMA_Tooling-main directory
    Not needed, data is being processed using Google Sheets currently

--- TDMA_Tooling-main/parse_utils.py
+++
@@ -1,9 +0,0 @@
-def parse_file(file, keys) -> list:
-    with open(file, "r") as file:
-        data = file.read()
-        raw = data.split('\n')
-        parsed = []
-    for item in raw:
-        parsed.append(item.split(","))
-    parsed = [dict(zip(keys, item)) for item in parsed]
-    return parsed
